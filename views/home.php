b2d000d6d3363153dbcbf45a3d4c82a97c1cebb8
Monica Craveiro <craveiromonica@gmail.com>
2020-04-05 00:48:05 -0300

    commit diff

--- views/home.php
+++ views/home.php
@@ -22,18 +22,18 @@
     <link rel="stylesheet" href="views/css/style.css">
 </head>
 <body>
-    <header>
+    <header id="header">
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="/fuseIoT"><object class="header-logo" data="views/img/LogoFusenav.svg" type="image/svg+xml"></object></a>
+            <a class="navbar-brand" href="/"><object class="header-logo" data="views/img/LogoFusenav.svg" type="image/svg+xml"></object></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav flex-row justify-content-center">
-                    <a class="nav-item nav-link text-body" href="#">IoT</a>
-                    <a class="nav-item nav-link text-body" href="#">Soluções</a>
-                    <a class="nav-item nav-link text-body" href="#">Contato</a>
+                    <a class="nav-item nav-link text-body" href="#iot">IoT</a>
+                    <a class="nav-item nav-link text-body" href="#solucoes">Soluções</a>
+                    <a class="nav-item nav-link text-body" href="#contato">Contato</a>
                 </div>
             </div>
         </div>
@@ -45,9 +45,10 @@
                 <div class="banner-lead">
                     <h1 class="banner-titulo titulo">Vamos fazer seu negócio gerar ainda mais resultados</h1>
                     <p class="banner-texto">A Fuse IoT é uma StartUp focada em tecnologias de IoT e Inteligência Artificial, que gera resultados com aprimoramento de processos visando a melhoria contínua do seu negócio.</p>
-                    <a href="" class="btn btn-padrao banner-btn btn-outline-success">Me ajude agora</a>
+                    <a href="#contato" class="btn btn-padrao banner-btn btn-outline-success">Me ajude agora</a>
                 </div>
             </div>
+            <div id="iotnav" style="margin-bottom: 5rem"></div>
         </section>
         <section id="iot">
             <div class="container text-center">
@@ -58,7 +59,7 @@
                     <div class="col-md-6 order-md-1">
                         <h5 class="titulo">Smartcities</h5>
                         <p class="verticais">Soluções para cidades conectadas. Monitore e controle a iluminação pública, lixeiras inteligentes, smart parking e muito mais.</p>
-                        <a href="" class="btn btn-padrao btn-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-success">Quero saber mais</a>
                     </div>
                     <div class="col-md-6 direita order-md-2">
                         <img class="bg-verticais" src="views/img/cities.svg" alt="">
@@ -66,7 +67,7 @@
                     <div class="col-md-6 order-md-4">
                         <h5 class="titulo">Agrobusiness</h5>
                         <p class="verticais">Aumente sua produtividade com monitoração climática e de solo, controle de temperatura e umidade, localização de ativos, rastreamento de bovino numa plataforma de gestão de fazendas.</p>
-                        <a href="" class="btn btn-padrao btn-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-success">Quero saber mais</a>
                     </div>
                     <div class="col-md-6 esquerda order-md-3">
                         <img class="bg-verticais" src="views/img/boi.svg" alt="">
@@ -74,7 +75,7 @@
                     <div class="col-md-6 order-md-5">
                         <h5 class="titulo">Health</h5>
                         <p class="verticais">Evite desperdícios, reduza custos e riscos controlando a temperatura e o funcionamento de geladeiras de vacinas e medicamentos, tecidos e amostras, bolsas de sangue e plasma, para ambientes hospitalares, clínicas, medicina diagnóstica e centros de pesquisa.</p>
-                        <a href="" class="btn btn-padrao btn-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-success">Quero saber mais</a>
                     </div>
                     <div class="col-md-6 direita order-md-6">
                         <img class="bg-verticais" src="views/img/health.svg" alt="">
@@ -82,7 +83,7 @@
                     <div class="col-md-6 order-md-8">
                         <h5 class="titulo">Indústria 4.0</h5>
                         <p class="verticais">Aumente a competitividade do seu negócio com melhora de eficiência operacional. Monitoração do funcionamento de máquinas e serviços otimizando manutenções preventivas, com controle de consumo elétrico, de água e gás.</p>
-                        <a href="" class="btn btn-padrao btn-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-success">Quero saber mais</a>
                     </div>
                     <div class="col-md-6 esquerda order-md-7">
                         <img class="bg-verticais" src="views/img/ind.svg" alt="">
@@ -97,7 +98,7 @@
                     <div class="carousel-texto">
                         <h4 class="titulo-solucao">Fuse Bov</h4>
                         <p class="texto-solucao">Solução para monitoramento e rastreamento de gado de corte gerando informações de localização, comportamento e comparativo com dados históricos compeltos de cada animal.</p>
-                        <a href="" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
                     </div>
                 </div>
                 <div class="solucao efeito utilities">
@@ -105,7 +106,7 @@
                     <div class="carousel-texto">
                         <h4 class="titulo-solucao">Fuse Utilities</h4>
                         <p class="texto-solucao mt-4 mb-md-4">Monitore o consumo de água, gás e <br>energia elétrica e evite desperdícios, identifique vazamentos, aumente a segurança no trabalho e reduza custos.<br></p>
-                        <a href="" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
                     </div>
                 </div>
                 <div class="solucao efeito temp">
@@ -113,7 +114,7 @@
                     <div class="carousel-texto">
                         <h4 class="titulo-solucao">Fuse Temp</h4>
                         <p class="texto-solucao">Solução completa para acompanhamento de temperatura, umidade, e contato binário (ligado/desligado, aberto/fechado), totalmente validável para hospitais e Inmetro.</p>
-                        <a href="" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
                     </div>
                 </div>
                 <div class="solucao efeito track">
@@ -121,7 +122,7 @@
                     <div class="carousel-texto">
                         <h4 class="titulo-solucao">Fuse Track</h4>
                         <p class="texto-solucao mt-4 mb-md-4">Saiba tudo sobre sua frota, rastreie seus ativos, otimize rotas, ganhe agilidade, reduza custos e tenha mais segurança sobre seus bens e serviços.</p>
-                        <a href="" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
                     </div>
                 </div>
                 <div class="solucao efeito parking">
@@ -129,7 +130,7 @@
                     <div class="carousel-texto">
                         <h4 class="titulo-solucao">Fuse Parking</h4>
                         <p class="texto-solucao mt-4">Monitore vagas de estacionamento ou locais de parada proibida, receba alertas em tempo real em caso de infração e controle o tráfego de veículos em locais de estudo.</p>
-                        <a href="" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
+                        <a href="#contato" class="btn btn-padrao btn-outline-success">Quero saber mais</a>
                     </div>
                 </div>
             </div>
@@ -145,7 +146,7 @@
             <div class="naoEncontrou">
                 <h3 class="naoEncontrou-titulo">Não encontrou o que você procurava?</h3>
                 <p class="naoEncontrou-texto">Customizamos soluções sob medida para sua necessidade. Entre em contato e te ajudaremos a tornar a sua imaginação o limite para resolver problemas com IoT!</p>
-                <a href="" class="btn btn-padrao btn-naoEncontrou btn-secondary">Quero uma solução customizada</a>
+                <a href="#contato" class="btn btn-padrao btn-naoEncontrou btn-secondary">Quero uma solução customizada</a>
             </div>
             <div class="container">
                 <div class="row">
@@ -153,7 +154,7 @@
                         <h3 class="titulo">Contate-nos agora mesmo</h3>
                         <form id="mailform" name="mailform">
                             <div class="form-group">
-                                <input value="monica.craveiro@fuseiot.io" name="toemail" type="text" hidden />
+                                <input value="murilo.silva@fuseiot.io" name="toemail" type="text" hidden />
                             </div>
                             <div class="form-group">
                                 <input value="contato@fuseiot.io" name="from" type="text" hidden />
@@ -201,71 +202,32 @@
                 </div>
             </div>
         </section>
-        <!-- <img src="views/img/logo-fuse.svg" class="logo" alt="Logo Fuse IoT">
-        <h1 class="title">SITE EM CONSTRUÇÃO</h1>
-        <p class="sub d-none d-md-block">Enquanto preparamos nosso  site, saiba mais sobre a Fuse IoT, uma StartUp especializada em soluções IoT, que fornece transformação digital para empresas e pessoas que querem melhorar seus indicativos!</P>
-        <button class="btn" data-toggle="modal" data-target="#modalContact">Entre em contato</button>
-        <div class="modal fade" id="modalContact" tabindex="-1" role="dialog" aria-labelledby="modalContactTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <h3>Quer saber mais?</h3>
-                        <p>Envie-nos seu contato pelo formulário abaixo:</p>
-                        <form id="mailform" name="mailform">
-                            <div class="form-group">
-                                <input value="msilva@nuveto.com.br" name="toemail" type="text" hidden />
-                            </div>
-                            <div class="form-group">
-                                <input value="contato@fuseiot.io" name="from" type="text" hidden />
-                            </div>
-                            <div class="form-group">
-                                <input value="Contato Site Fuse | IoT" name="subject" type="text" hidden />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="nome" id="nome" placeholder="Seu nome" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Seu e-mail" required>
-                            </div>
-                            <div class="form-group">
-                                <textarea class="form-control" id="message" name="message" rows="4" placeholder="Sua mensagem"></textarea>
-                            </div>
-                            <button type="button" id="sendemail" class="btn" onclick="GoSend();" data-dismiss="modal" aria-label="Fechar" >Enviar</button>
-                            <div class="form-group">
-                                <select class="form-control" name="sendmethod" hidden >
-                                    <option value="smtp" selected >SMTP</option>
-                                </select>
-                            </div>
-                        </form>
+        <section>
+            <div class="modal fade" id="modalOk" tabindex="-1" role="dialog" aria-labelledby="modalOkTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <h3>Pronto!</h3>
+                            <p>Em breve nossos especialistas irão entrar em contato para dar mais informações.</p>
+                            <a class="btn btn-padrao btn-success text-white" data-dismiss="modal" aria-label="Fechar">OK</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="modal fade" id="modalOk" tabindex="-1" role="dialog" aria-labelledby="modalOkTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <h3>Pronto!</h3>
-                        <p>Em breve nossos especialistas irão entrar em contato para dar mais informações.</p>
-                        <a class="btn" data-dismiss="modal" aria-label="Fechar">OK</a>
+            <div class="modal fade" id="modalErro" tabindex="-1" role="dialog" aria-labelledby="modalErroTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <h3>Erro!</h3>
+                            <p><?= "Houve um erro enviando o email. Tente de novo mais tarde!" ?></p>
+                            <a class="btn btn-padrao btn-success text-white" data-dismiss="modal" aria-label="Fechar">OK</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="modal fade" id="modalErro" tabindex="-1" role="dialog" aria-labelledby="modalErroTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <h3>Erro!</h3>
-                        <p><?= "Houve um erro enviando o email. Tente de novo mais tarde!" ?></p>
-                        <a class="btn" data-dismiss="modal" aria-label="Fechar">OK</a>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
+        </section>
+        
         <table hidden id="msglog" border="1" bordercolor="#FFCC00" style="background-color:#FFFFCC" width="100%" cellpadding="3" cellspacing="3">
             <tr>
                 <td>#</td>
@@ -286,8 +248,8 @@
             <div class="footer-cima">
                 <object class="footer-logo" data="views/img/LogoFusefooter.svg" type="image/svg+xml"></object>
                 <ul class="footer-politica">
-                    <li class=""><a href="" class="footer-link">Política de Privacidade</a></li>
-                    <li class=""><a href="" class="footer-link">Informações Legais</a></li>
+                    <li class=""><a href="/" class="footer-link">Política de Privacidade</a></li>
+                    <li class=""><a href="/" class="footer-link">Informações Legais</a></li>
                 </ul>
             </div>
             <hr>
@@ -299,6 +261,12 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="views/js/carousel.js"></script>
+    <script type="text/javascript">
+        var divHeight;
+        var obj = document.querySelector('#header').outerHeight(true);
+        var body = document.querySelector('body');
+        body.setAttribute("style", `margin-top: ${obj}px`);
+    </script>
     <script>
         var msgid = 1;
         function GoSend() {
@@ -357,11 +325,9 @@
                         } else {
                             RESULTcell.innerHTML="HTTP/1.1 "+request.status+" "+request.statusText+"<br /><br />"+request.responseText;
                             if(request.status == 200){
-                                // $('#modalOk').modal('show');
-                                console.log("ok");
+                                $('#modalOk').modal('show');
                             }else{
-                                // $('#modalErro').modal('show');
-                                console.log("erro");
+                                $('#modalErro').modal('show');
                             }
                         }
                     }
